530f6cc731e74abf6a298cde901f0f98fee265ab
timokonkwo <chibuiketimokonkwo@gmail.com>
2022-08-11 12:21:38 +0100

    implemented adding chats from DOM to database

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,17 @@
 // DOM queries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+// Add a new chat
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err))
+
+})
 
 
 // Class instances
--- scripts/ui.js
+++ scripts/ui.js
@@ -13,7 +13,7 @@ class ChatUI {
         )
         const html = `
             <li class="list-group-item">
-                <span class="username">${data.username}</span>
+                <span class="username">${data.username}:</span>
                 <span class="message">${data.message}</span>
                 <div class="time">${when} ago</div>
             </li>
